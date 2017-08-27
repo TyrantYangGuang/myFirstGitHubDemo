d1b359f3aa70e6ef30566b138308e18cdc72fff4
lql1989 <qlian_liang@163.com>
2017-08-27 11:48:48 +0800

    解决了冲突

--- src/cm/lql/dao/ICar.java
+++ src/cm/lql/dao/ICar.java
@@ -3,13 +3,10 @@ package cm.lql.dao;
 public interface ICar {
 
 	public void bayCar();
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 	public void baySomething();
-=======
+
 	public void byaaa();
->>>>>>> addOneClass
-=======
-	public void byaaa();
->>>>>>> addOneClass
+
+
 }
